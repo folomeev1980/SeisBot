4b90e5a42dcea196df882a3b5326de4a41a8e63e
folomeev1980 <folomeev80@gmail.com>
2018-08-07 10:50:07 +0300

    ad #test

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, "Привет Александр")
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
