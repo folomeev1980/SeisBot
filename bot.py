6cd3c57f15308fc17008e4987cf908c3ce06ad78
folomeev1980 <40419806+folomeev1980@users.noreply.github.com>
2018-08-07 06:44:19 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+
+#main variables
+TOKEN = '513308297:AAFxvSsa6hDNk238pON8i3j-nOGSlmygitU'
+
+
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
