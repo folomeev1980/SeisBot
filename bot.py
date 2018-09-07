678fd74e220a0cbba0b6965a7de0db2d1960680c
folomeev1980 <folomeev80@gmail.com>
2018-09-07 18:01:56 +0300

    edition 7 _webhook

--- bot.py
+++ bot.py
@@ -2,13 +2,15 @@ import telebot
 import scftool
 import smngtool
 import config
+import logging
 import urllib
 import valut
 import adultsender
 
 import os
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 
 TOKEN = os.environ.get('TOKEN', config.TOKEN)
@@ -19,10 +21,26 @@ PORT = int(os.environ.get('PORT', '5000'))
 
 
 def echo(bot, update):
-    update.message.reply_text('Bot answer: ' + update.message.text)
+    update.message.reply_text(config.help)
+
+def vl(bot,update):
+    update.message.reply_text(valut.valuta)
+
+
+
+
 
 
 updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+
+
+vl_handler = CommandHandler('vl', vl)
+dispatcher.add_handler(vl_handler)
+
+
+
 
 # add handlers
 updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
