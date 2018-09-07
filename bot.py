2cb88bc4294aa05258861b5628fb247f2dc3c307
folomeev1980 <folomeev80@gmail.com>
2018-09-07 13:03:46 +0300

    edition 6

--- bot.py
+++ bot.py
@@ -132,7 +132,7 @@ def repeat_all_messages(message):
 
 
 #if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=port)
+server.run(host='0.0.0.0', port=port)
 '''
     bot.polling(none_stop=True, interval=5)
    '''
