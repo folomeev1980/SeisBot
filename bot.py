89d2abb3c6e41a5ecacaaec015c9d9d6fedecf08
folomeev1980 <folomeev80@gmail.com>
2018-09-07 12:46:16 +0300

    edition 5

--- bot.py
+++ bot.py
@@ -5,10 +5,25 @@ import config
 import urllib
 import valut
 import adultsender
-
+import os
+from flask import Flask, request
+server = Flask(__name__)
 bot = telebot.TeleBot(config.TOKEN)
+port = int(os.environ.get("PORT", 5000))
 
+#---------------------------------------------------------
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://seisbot.herokuapp.com/bot") #ссылку изменил
+    return "!", 200
 
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_messages(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message])
+    return "!", 200
+#---------------------------------------------------------------------------------------
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, config.help)
@@ -115,6 +130,8 @@ def send_adult5(message):
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, config.help)
 
-
+'''
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=5)
+'''
+server.run(host='0.0.0.0', port=port)
