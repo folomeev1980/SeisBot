62611d56ad2e979526fc4a22a28da89d917071a4
folomeev1980 <Lazarev0205!980>
2020-07-11 23:41:28 +0300

    webdriver

--- config.py
+++ config.py
@@ -34,6 +34,7 @@ def get_ship_info(vessel_name,url):
     #print(soup)
 
     tds = soup.find("div",class_="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-true")
+    print(soup)
     tds=tds.findAll("p")
 
 
