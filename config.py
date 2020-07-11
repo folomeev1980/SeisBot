b8ba3c4bf00c6d7f61982cad4a27375a3ac96448
folomeev1980 <Lazarev0205!980>
2020-07-11 23:39:37 +0300

    webdriver

--- config.py
+++ config.py
@@ -36,6 +36,8 @@ def get_ship_info(vessel_name,url):
     tds = soup.find("div",class_="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-true")
     tds=tds.findAll("p")
 
+
+
     for i in tds:
         if "Position Received" in str(i.text):
        #     print(i.text)
