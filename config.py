3249f09c59320268ed3c37a99c7dd72c9bf57536
folomeev1980 <Lazarev0205!980>
2020-07-12 13:58:35 +0300

    webdriver

--- config.py
+++ config.py
@@ -24,9 +24,9 @@ def get_ship_info(vessel_name,url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument('window-size=1920x935')
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    #chrome_options.add_argument('window-size=1920x935')
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
     html = driver.page_source
